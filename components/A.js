da8c340a7a737335014fae28b27c4d6f8b4fe849
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-09-11 01:48:58 +0300

    c

--- components/A.js
+++ components/A.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import style from '../styles/A.module.css'
 export default function A({ href, text }) {
   return (
+    
     <Link href={href}>
       <a className={style.link}>{text}</a>
     </Link>
